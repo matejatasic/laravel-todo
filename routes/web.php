a2433c8960be19e6b2b4bfe576dd5bd303d7e12c
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-09-21 15:43:29 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,5 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/todo/index', [TodoController::class, 'index'])->name('todo.index');
 
 Auth::routes();
