fc80d868bb9501694ddfd91a445e4d82c5c19d62
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-09-22 11:22:07 +0200

    Update create.blade.php

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -1,5 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Create Task</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('todos.store') }}">
+                            @csrf
+
+                            <div class="form-group p-2 row">
+                                <label>Description</label>
+                                <textarea name="description" class="form-control" cols="30" rows="5"></textarea>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-8 offset-md-5">
+                                    <button type="submit" class="btn btn-primary">Create</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
