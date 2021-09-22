d511c5d358a3827941665dde8a6bc5be8732f76b
Mateja Tasic <70795666+matejatasic@users.noreply.github.com>
2021-09-22 13:50:40 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <div class="jumbotron">
         <div class="container">
             <h1 class="display-3">Welcome!</h1>
-            <p>This is a simple Laravel Todo app. With it you will be able to make a todo list, check, uncheck and delete todos. Click the button below to start making your todo list.</p>
+            <p>This is a simple Laravel Todo app. With it you will be able to make a todo list, create, edit and delete tasks. Click the button below to start making your todo list.</p>
             <p><a class="btn btn-primary btn-lg" href="{{ route('todos.index') }}" role="button">Start &#62;</a></p>
         </div>
     </div>
